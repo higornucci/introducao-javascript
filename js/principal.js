193778957942e818d01889998dbc85e7291c6458
Higor Nucci <higornucci@gmail.com>
2019-07-10 15:27:08 -0400

    calculo do imc

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,33 @@
 let titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+let tdImc = paciente.querySelector(".info-imc");
+
+let pesoEhValido = true;
+let alturaEhValida = true;
+
+if (peso < 0 || peso >= 1000) {
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso inválido!";
+}
+
+if (altura < 0 || altura >= 3.00){
+    console.log("Altura inválida!");
+    alturaEhValida = false;
+    tdImc.textContent = "Altura inválida!";
+}
+
+if (alturaEhValida && pesoEhValido) {
+    tdImc.textContent = peso / (altura * altura);
+} else {
+    tdImc.textContent = "Altura e/ou peso inválidos!"
+}
